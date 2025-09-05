02927ca810965a08af2274f5622a049582d7b21a
DatLmao <datnguyendat2006@gmail.com>
2025-09-05 14:45:37 +0700

    style for card

--- Weather App/script.js
+++
@@ -1,8 +0,0 @@
-const courses = [1, 2, 3, 4];
-const res = courses.map(course => {
-    return `<li>${course}</li>`;
-});
-console.log(res);
-const html = `<ul>${res.join("\n")}$</ul>`
-const output = document.querySelector(`#op`);
-output.innerHTML = html;
