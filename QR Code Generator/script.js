d5a204385befc40aefcb35a906c98c72cb0f2222
DatLmao <datnguyendat2006@gmail.com>
2025-09-09 22:12:35 +0700

    style

---
+++ QR Code Generator/script.js
@@ -0,0 +1,7 @@
+const imgBox = document.getElementById("imgBox");
+const qrImg = document.getElementById("qrImg");
+const qrText = document.getElementById("qrText");
+
+const generateQR = () => {
+      
+}
